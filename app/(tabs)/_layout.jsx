50b73749ef650d1396d0a71a6c0d514475bf1c4e
shaheem <shaheemshedid4@gmail.com>
2024-12-22 01:25:13 -0500

    tabs color theme bug fix

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import { Tabs } from "expo-router";
 import { FontAwesome } from "@expo/vector-icons";
-import {ThemeProvider} from "../../contexts/ThemeContext";
+import {useTheme} from "../../contexts/ThemeContext";
+import {colorsPalette} from "../../assets/colorsPalette";
 
 export default function TabsLayout() {
+    const { theme } = useTheme()
+    const colors = colorsPalette[theme];
     return (
-            <Tabs>
+            <Tabs screenOptions={{
+                tabBarStyle: {
+                    backgroundColor: colors.background_c1, // Set the tab bar background color
+                },
+                tabBarActiveTintColor: colors.secondary, // Active tab text/icon color
+                tabBarInactiveTintColor: '#888888', // Inactive tab text/icon color
+                headerStyle: {
+                    backgroundColor: colors.background_c1, // Set the header background color
+                },
+                headerTintColor: colors.text, // Set the header text/icon color
+            }}>
                 <Tabs.Screen
                     name="movies"
                     options={{
@@ -14,6 +27,7 @@ export default function TabsLayout() {
                             <FontAwesome name="film" color={color} size={size} />
                         ),
                     }}
+
                 />
                 <Tabs.Screen
                     name="[user]/profile"
