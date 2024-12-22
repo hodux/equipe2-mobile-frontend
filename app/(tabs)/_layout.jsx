72011d093ec811a71bbf0960d4deb636569a9512
hodux <hoduxs@gmail.com>
2024-12-22 00:01:29 -0500

    feat: add animations to movie grid

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,28 +1,31 @@
 import React from "react";
 import { Tabs } from "expo-router";
 import { FontAwesome } from "@expo/vector-icons";
+import {ThemeProvider} from "../../contexts/ThemeContext";
 
 export default function TabsLayout() {
     return (
-        <Tabs>
-            <Tabs.Screen
-                name="movies"
-                options={{
-                    tabBarLabel: "Movies",
-                    tabBarIcon: ({ color, size }) => (
-                        <FontAwesome name="film" color={color} size={size} />
-                    ),
-                }}
-            />
-            <Tabs.Screen
-                name="profile"
-                options={{
-                    tabBarLabel: "Profile",
-                    tabBarIcon: ({ color, size }) => (
-                        <FontAwesome name="user" color={color} size={size} />
-                    ),
-                }}
-            />
-        </Tabs>
+        <ThemeProvider>
+            <Tabs>
+                <Tabs.Screen
+                    name="movies"
+                    options={{
+                        tabBarLabel: "Movies",
+                        tabBarIcon: ({ color, size }) => (
+                            <FontAwesome name="film" color={color} size={size} />
+                        ),
+                    }}
+                />
+                <Tabs.Screen
+                    name="profile"
+                    options={{
+                        tabBarLabel: "Profile",
+                        tabBarIcon: ({ color, size }) => (
+                            <FontAwesome name="user" color={color} size={size} />
+                        ),
+                    }}
+                />
+            </Tabs>
+        </ThemeProvider>
     );
 }
